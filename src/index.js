76aba1876cb5d1f992affe6d580b8b3e2576c511
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-01-07 17:00:08 +0530

    Added App.jsx Added App.jsx and exported so that it is easy to use and easy read each component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading.jsx";
-import List from "./List.jsx";
+import App from "./App.jsx";
 
-ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
